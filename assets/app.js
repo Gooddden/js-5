65868620779b20245930e6f0e2f0cb10844f5420
Gooddden <goodden86@gmail.com>
2023-04-20 09:42:50 +0300

    1

---
+++ assets/app.js
@@ -0,0 +1,27 @@
+
+function calc(){
+
+    let weight =    +userWeight.velue;
+
+    let heigt =     +userHeight.velue / 100;
+    
+    let bmi = weight / heigt **2;
+   
+    result.innerHTML = bmi;
+
+    if(bmi <= 16){
+        meaning.innerHTML = 'Выраженный (дефицит) массы тела' ;
+    } else if(bmi < 18.5 ){
+        meaning.innerHTML = 'Недостаточная масса тела';
+    } else if(bmi < 25 ){
+        meaning.innerHTML = 'Норма';
+    } else if(bmi < 30 ){
+        meaning.innerHTML = 'Избыточная масса тела (предожирение)';
+    } else if(bmi < 35 ){
+        meaning.innerHTML = 'Ожирение 1 степени';
+    } else if(bmi < 40 ){
+        meaning.innerHTML = 'Ожирение 2 степени';
+    } else if(bmi > 40 ){
+        meaning.innerHTML = 'Ожирение 3 степени';
+    } 
+}
